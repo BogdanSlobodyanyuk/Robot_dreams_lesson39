d32005a481de85c42c9e4224e3a2cea9657112bc
Bogdan <bogdan.slobodyanyuk.1998@gmail.com>
2023-05-15 23:10:51 +0100

    Initial commit

---
+++ src/main/java/com/ua/robot/RobotDreamsLesson39Application.java
@@ -0,0 +1,13 @@
+package com.ua.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotDreamsLesson39Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotDreamsLesson39Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ua/robot/RobotDreamsLesson39ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotDreamsLesson39ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
